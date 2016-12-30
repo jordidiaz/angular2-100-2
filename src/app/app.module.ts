11dfbd874a49ebdc71ac5e939343eab1615f1df3
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2016-12-30 16:38:29 +0100

    Crazy AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,100 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent} from './app.component';
+
+import { AppComponent as A1Component} from './app.component';
+import { AppComponent as A2Component} from './app.component';
+import { AppComponent as A3Component} from './app.component';
+import { AppComponent as A4Component} from './app.component';
+import { AppComponent as A5Component} from './app.component';
+import { AppComponent as A6Component} from './app.component';
+import { AppComponent as A7Component} from './app.component';
+import { AppComponent as A8Component} from './app.component';
+import { AppComponent as A9Component} from './app.component';
+import { AppComponent as A10Component} from './app.component';
+
+import { AppComponent as A1Pipe} from './app.component';
+import { AppComponent as A2Pipe} from './app.component';
+import { AppComponent as A3Pipe} from './app.component';
+import { AppComponent as A4Pipe} from './app.component';
+import { AppComponent as A5Pipe} from './app.component';
+import { AppComponent as A6Pipe} from './app.component';
+import { AppComponent as A7Pipe} from './app.component';
+import { AppComponent as A8Pipe} from './app.component';
+import { AppComponent as A9Pipe} from './app.component';
+import { AppComponent as A10Pipe} from './app.component';
+
+import { AppComponent as A1Module} from './app.component';
+import { AppComponent as A2Module} from './app.component';
+import { AppComponent as A3Module} from './app.component';
+import { AppComponent as A4Module} from './app.component';
+import { AppComponent as A5Module} from './app.component';
+import { AppComponent as A6Module} from './app.component';
+import { AppComponent as A7Module} from './app.component';
+import { AppComponent as A8Module} from './app.component';
+import { AppComponent as A9Module} from './app.component';
+import { AppComponent as A10Module} from './app.component';
+
+import { AppComponent as A1Service} from './app.component';
+import { AppComponent as A2Service} from './app.component';
+import { AppComponent as A3Service} from './app.component';
+import { AppComponent as A4Service} from './app.component';
+import { AppComponent as A5Service} from './app.component';
+import { AppComponent as A6Service} from './app.component';
+import { AppComponent as A7Service} from './app.component';
+import { AppComponent as A8Service} from './app.component';
+import { AppComponent as A9Service} from './app.component';
+import { AppComponent as A10Service} from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    A1Component,
+    A2Component,
+    A3Component,
+    A4Component,
+    A5Component,
+    A6Component,
+    A7Component,
+    A8Component,
+    A9Component,
+    A10Component,
+    A1Pipe,
+    A2Pipe,
+    A3Pipe,
+    A4Pipe,
+    A5Pipe,
+    A6Pipe,
+    A7Pipe,
+    A8Pipe,
+    A9Pipe,
+    A10Pipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    A1Module,
+    A2Module,
+    A3Module,
+    A4Module,
+    A5Module,
+    A6Module,
+    A7Module,
+    A8Module,
+    A9Module,
+    A10Module
   ],
-  providers: [],
+  providers: [
+    A1Service,
+    A2Service,
+    A3Service,
+    A4Service,
+    A5Service,
+    A6Service,
+    A7Service,
+    A8Service,
+    A9Service,
+    A10Service
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
